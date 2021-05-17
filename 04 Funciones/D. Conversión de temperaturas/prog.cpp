39df925d6b31095d88b340458d807d5a64dcd94a
Gerardo Beltran <contacto@gerardobeltran.mx>
2021-05-17 10:47:54 -0600

    .

--- 04 Funciones/D. Conversión de temperaturas/prog.cpp
+++ 04 Funciones/D. Conversión de temperaturas/prog.cpp
@@ -4,12 +4,26 @@ using namespace std;
 int Kelvin(int c);
 int Fahrenheit(int c);
 int Reaumur(int c);
+void Temperaturas(int c);
+
 int main() {
 
    int c;
    cin >> c;
-   cout << Kelvin(c) << " " << Fahrenheit(c) << " " << Reaumur (c) << endl;
-   return 0; 
+//    cout << Kelvin(c) << " " << Fahrenheit(c) << " " << Reaumur (c) << endl;
+
+   Temperaturas(c);
+
+  return 0;
+}
+
+void  Temperaturas(int c){
+  int k= c + 273;
+  int  f= (1.8 * c) + 32;
+  int r= c * 0.8;
+
+  cout << k << " " << f << " " << r << endl;
+
 }
 
 int Kelvin(int c){
