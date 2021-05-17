580ae617ea0630e6cd55854172f6855a1a92f174
Gerardo Beltran <contacto@gerardobeltran.mx>
2021-05-17 10:33:42 -0600

    .

--- 04 Funciones/D. Conversión de temperaturas/prog.cpp
+++ 04 Funciones/D. Conversión de temperaturas/prog.cpp
@@ -5,6 +5,20 @@ int main() {
 
 
 
-
    return 0;
+}
+
+int Kelvin(int c){
+  int k= c + 273;
+  return k;
+}
+
+int Fahrenheit(int c){
+ int  f= (1.8 * c) + 32;
+ return f; 
+}
+
+int Reaumur(int c){
+  int r= c * 0.8;
+  return r;
 }
