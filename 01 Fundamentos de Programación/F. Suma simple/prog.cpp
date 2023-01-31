d483c994f366a2af22e3f2126297ce65d288a070
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-01-31 17:57:17 +0000

    --

---
+++ 01 Fundamentos de Programación/F. Suma simple/prog.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    //cout << "Escriba un numero: ";
+    cin >> num;
+    
+    num = num + 1;
+    
+    cout << num << endl;
+    
+    return 0;
+}
