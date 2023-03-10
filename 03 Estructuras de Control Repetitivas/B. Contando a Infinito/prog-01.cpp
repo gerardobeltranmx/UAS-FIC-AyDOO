3f32adea2b161d53a79a5e8e638d1db136263576
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-03-10 10:14:14 -0700

    --

---
+++ 03 Estructuras de Control Repetitivas/B. Contando a Infinito/prog-01.cpp
@@ -0,0 +1,18 @@
+    #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+   // cout << "Indicar el numero: ";
+    cin >> n;
+    
+    for (int i=1; i<=n; i++){
+        cout << i << endl;
+    }
+
+
+    return 0;
+}
