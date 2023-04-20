a191ffee992fe10dea371a7327e3153ff46e29f6
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-04-20 10:47:46 -0700

    --

--- 04 Programación Modular/E. Cilindro/prog.cpp
+++ 04 Programación Modular/E. Cilindro/prog.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<iomanip>
+
 using namespace std;
 
 #define PI 3.1416
@@ -7,7 +9,6 @@ double areaCirculo (double radio){
     double area;
     area = PI * radio * radio;
 }
-
 double perimetroCirculo(double radio){
     double perimetro ;
     perimetro = 2 * PI * radio;
@@ -18,26 +19,26 @@ double areaRectangulo (double lado1, double lado2){
     area = lado1 * lado2;
     return area;
 }
-
 double areaCilindro (double radio, double altura){
     double area;
     area = 2 * areaCirculo(radio);
     area = area +  perimetroCirculo(radio) * altura;
     return area;
 }
-
 double volumenCilindro (double radio, double altura){
     double volumen;
     volumen = areaCirculo(radio) * altura;
     return volumen;
 }
 
-
 int main(){
-
-
-
-
-
+    double r, a, volumen, area;
+    cin >> r;
+    cin >> a;
+    area = areaCilindro(r, a);
+    volumen = volumenCilindro(r, a);
+    cout <<fixed << setprecision(2)<< "AREA=" << area << endl;
+    cout <<fixed << setprecision(2)<< "VOLUMEN=" << volumen << endl;
+ 
     return 0;
 }
