43ed7f94dfefbb68645894d6d754959e478ce1cc
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-05-11 10:58:13 -0700

    --

---
+++ 05. Arreglos/D. Suma de vectores/prog-03.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int N;
+int a1[100], a2[100], a3[100];
+int dato, suma=0;
+// Entrada al cantidad de elementos de los arreglos
+cin >> N;
+// Entrada a datos del primer arreglo
+for (int i=0; i < N; i++){
+    cin >> a1[i]; 
+}
+// Entrada a datos del segundo arreglo
+for (int i=0; i < N; i++){
+    //cin >> a2[i]; 
+    cin >> dato; 
+
+    suma = a1[i] + dato;
+       cout << suma << " ";
+}
+// suma ambos arreglos
+//for (int i=0; i < N; i++){
+//    a3[i] = a1[i] + a2[i];
+//}
+// Generamos salida de a3
+//for (int i=0; i <N; i++){
+//  cout << a3[i] << " ";
+//}
+cout << endl;
+
+
+
+
+
+
+
+
+
+    return 0;
+}
